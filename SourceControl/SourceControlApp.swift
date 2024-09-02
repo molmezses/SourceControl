de7e631f79a337233dfa41e36a1fe8001cd8d46d
Mustafa <mustafaolmezses@icloud.com>
2024-09-02 23:17:02 +0300

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Mustafa Ölmezses on 2.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
