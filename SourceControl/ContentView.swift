793234d5a03a1b268c7b2d8ad0dab12ee24d5f42
Mustafa <mustafaolmezses@icloud.com>
2024-09-03 17:10:36 +0300

    My second commit

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("Mustafa Ölmezses")
+            
+            Button("Click me") {
+                
+            }
         }
         .padding()
     }
