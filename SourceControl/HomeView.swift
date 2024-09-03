5ca083f912992d81fadc34df697b9dcc028e5bf1
Mustafa <mustafaolmezses@icloud.com>
2024-09-03 17:18:58 +0300

    Add HomeView

---
+++ SourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SourceControl
+//
+//  Created by Mustafa Ölmezses on 3.09.2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Second Screen")
+    }
+}
+
+#Preview {
+    HomeView()
+}
